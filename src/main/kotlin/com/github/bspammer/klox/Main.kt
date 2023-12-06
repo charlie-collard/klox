795424c6dbfacb43c149258db7d31c42f540bd08
Charlie Collard <charliecollard0@gmail.com>
2023-12-06 16:07:18 +0000

    4.2 Token classes

--- src/main/kotlin/com/github/bspammer/klox/Main.kt
+++ src/main/kotlin/com/github/bspammer/klox/Main.kt
@@ -1,3 +1,5 @@
+package com.github.bspammer.klox
+
 import java.io.BufferedReader
 import java.io.File
 import java.io.InputStreamReader
---
+++ src/main/kotlin/com/github/bspammer/klox/Token.kt
@@ -0,0 +1,12 @@
+package com.github.bspammer.klox
+
+data class Token(
+    val tokenType: TokenType,
+    val lexeme: String,
+    val literal: Any,
+    val line: Int,
+) {
+    override fun toString(): String {
+        return "$tokenType $lexeme $literal"
+    }
+}
